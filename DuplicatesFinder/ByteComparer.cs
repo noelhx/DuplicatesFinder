3d79cc2cbaca6a5ac25facf49a1cef42cda2b774
Vitaly Dudarev <vitalydudarev@gmail.com>
2014-12-28 15:57:47 +0300

    added initial implementation of byte-by-byte comparer.

---
+++ DuplicatesFinder/ByteComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace DuplicatesFinder
+{
+    public class ByteComparer : IComparer
+    {
+        public bool Equals(string fileName1, string fileName2)
+        {
+            using (var stream1 = File.OpenRead(fileName1))
+            using (var stream2 = File.OpenRead(fileName2))
+            {
+                // to be implemented
+            }
+
+            return false;
+        }
+    }
+}
+
