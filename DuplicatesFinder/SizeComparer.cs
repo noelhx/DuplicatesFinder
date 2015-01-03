cd99a0d9019555d6a0e6f68ec7be37844b71b639
Vitaly Dudarev <vitalydudarev@gmail.com>
2015-01-03 15:00:34 +0300

    added caching of FileInfo in SizeComparer for better performance.

--- DuplicatesFinder/SizeComparer.cs
+++ DuplicatesFinder/SizeComparer.cs
@@ -1,9 +1,18 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace DuplicatesFinder
 {
     public class SizeComparer : IComparer
     {
+        #region Private Fields
+
+        private Dictionary<string, FileInfo> _cache = new Dictionary<string, FileInfo>(); 
+
+        #endregion
+
+        #region Public Methods
+
         public bool Equals(string fileName1, string fileName2)
         {
             var size1 = GetFileSize(fileName1);
@@ -12,10 +21,26 @@ namespace DuplicatesFinder
             return size1.Equals(size2);
         }
 
+        #endregion
+
+        #region Private Methods
+
         private long GetFileSize(string fileName)
         {
-            var fi = new FileInfo(fileName);
-            return fi.Length;
+            var fileInfo = GetOrInitFileInfo(fileName);
+            return fileInfo.Length;
         }
+
+        private FileInfo GetOrInitFileInfo(string fileName)
+        {
+            FileInfo fileInfo;
+
+            if (!_cache.TryGetValue(fileName, out fileInfo))
+                fileInfo = new FileInfo(fileName);
+
+            return fileInfo;
+        }
+
+        #endregion
     }
 }
