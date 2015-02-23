c4f63e7e7a8145f6efc9b229ec339243687e4bc5
Vitaly Dudarev <vitalydudarev@gmail.com>
2015-02-23 21:28:18 +0300

    added ScanOptions.

---
+++ DuplicatesFinder/ScanOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DuplicatesFinder
+{
+    public class ScanOptions
+    {
+        #region Public Properties
+
+        public FileCompareType FileCompareType { get; set; }
+        public bool IncludeSubDirectories { get; set; }
+        public string SearchPattern { get; set; }
+
+        #endregion
+    }
+}
+
